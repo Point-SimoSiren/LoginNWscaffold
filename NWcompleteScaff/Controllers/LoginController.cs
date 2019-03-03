a0f38f1d71dc7d7a52b664da008837863679ab8a
Simo Siren <simo.siren@stud.pointcollege.fi>
2019-03-03 11:01:02 +0200

    Customers basic scaffolding made, Login system not completely ready yet.

--- NWcompleteScaff/Controllers/LoginController.cs
+++ NWcompleteScaff/Controllers/LoginController.cs
@@ -3,26 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data;
 
 namespace NWcompleteScaff.Controllers
 {
-    public class HomeController : Controller
+    public class LoginController : Controller
     {
+        
+
+        // GET: Login
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        [HttpPost]
+        public ActionResult Autherize(NWcompleteScaff.Models.User userModel)
         {
-            ViewBag.Message = "Your application description page.";
 
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+            
+                
 
             return View();
         }
